5f8314376ebb6e5a8099253ec79fd40ea835beac
ldng <ludovic@izberg-marketplace.com>
2021-12-22 12:04:29 +0000

    [MISC] Drop service (#11)
    * [MISC] Drop service
    
    * Bump version to 1.7.0

--- icebergsdk/resources/__init__.py
+++ icebergsdk/resources/__init__.py
@@ -21,7 +21,6 @@ from icebergsdk.resources.currency import Currency
 from icebergsdk.resources.mp_admin import Transaction, MarketPlaceTransaction
 from icebergsdk.resources.return_refund import Return, Refund
 from icebergsdk.resources.channels import ProductChannel, ChannelPropagationPolicy, ProductChannelLogEvent
-from icebergsdk.resources.service import ServiceOffer, ServiceOfferVariation, ServiceOption
 from icebergsdk.resources.timeslots import (
     AvailabilityCalendar, AvailabilityTimeSlot, Reservation)
 from icebergsdk.resources.options import Option, OptionAnswer
@@ -82,9 +81,6 @@ def get_class_from_resource_uri(resource_uri):
         "product_channel_log_event": ProductChannelLogEvent,
         "product_family": ProductFamily,
         "product_family_selector": ProductFamilySelector,
-        "service_offer": ServiceOffer,
-        "service_option": ServiceOption,
-        "service_offer_variation": ServiceOfferVariation,
         "availability_calendar": AvailabilityCalendar,
         "availability_timeslot": AvailabilityTimeSlot,
         "reservation": Reservation,
